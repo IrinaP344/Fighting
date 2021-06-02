367c7f99389d18b69fb46924eaf8b1f73848a709
IrinaP344 <71124960+IrinaP344@users.noreply.github.com>
2021-06-02 09:59:11 +0900

    Add files via upload

---
+++ BodyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyController : MonoBehaviour
+{
+	float x;
+	float y; 
+
+	Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Vertical");
+        y = Input.GetAxis("Horizontal");
+
+        rb.velocity = transform.forward * x * 10f;
+    }
+}
---
+++ EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+	NavMeshAgent move;
+	public GameObject player;
+	Transform player_transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = GetComponent<NavMeshAgent>();
+        player_transform = player.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move.SetDestination(player_transform.position);
+    }
+
+    void OnCollisionEnter(Collision collision_object)
+    {
+    	print("столкновение есть");
+    	if(collision_object.gameObject.tag =="enemy")
+    	{
+    		Destroy(collision_object.gameObject);
+    	}
+    }
+}
